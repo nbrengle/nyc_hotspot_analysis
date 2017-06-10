11514d2963fe939e436aed72ccd097db3a79d442
nathaniel <nathaniel@playstudios.com>
2017-06-10 12:20:37 -0700

    hacky workaround on SpaceTimeCoordinates to nix bad data

--- src/main/scala/LineParser.scala
+++ src/main/scala/LineParser.scala
@@ -9,21 +9,39 @@ class LineParser(precision: Double = 0.1, timestep: Int = 3) extends java.io.Ser
 
     val LAT_OFFSET = 40.5000000
     val LONG_OFFSET = -74.25000000
-    val DATETIME_OFFSET = LocalDateTime.of(2015,1,1,0,0,0)
+    val START_DATE = "2015-01-01 00:00:00"
+    val DATETIME_OFFSET = setupDateOffset(START_DATE)
     val DATE_COL = 2
     val LAT_COL = 10
     val LONG_COL = 9
     //this value literally doesn't matter, just using a const to get the desired form
     val numPassenger = 1
 
+    def setupDateOffset(date: String): LocalDateTime = {
+        val formatter = DateTimeFormatter.ofPattern("uuuu-MM-DD HH:mm:ss")
+        return LocalDateTime.parse(START_DATE, formatter)
+    }
+
     def parseLine(line: Array[String]): Map[SpaceTimeCoordinate, Int] = {
         return Map(parseCoord(line) -> numPassenger)
     }
 
     def parseCoord(line: Array[String]): SpaceTimeCoordinate = {
-        val date = parseDate(line(DATE_COL).toString, timestep)
-        val lat = parseLat(line(LAT_COL).toDouble, precision)
-        val lon = parseLong(line(LONG_COL).toDouble, precision)
+        //Ugly workaround because Java Date Parsing handles 2015-02-01 00:13:14 incorrectly
+        val date = try {
+            parseDate(line(DATE_COL).toString, timestep)
+        } catch {
+            case ioe: DateTimeParseException => -1
+        }
+
+        val lat_in = line(LAT_COL).toDouble
+        val lon_in = line(LONG_COL).toDouble
+        if (date == -1 || lat_in == 0.0 || lon_in == 0.0) {
+            Unit
+        }
+
+        val lat = parseLat(lat_in, precision)
+        val lon = parseLong(lon_in, precision)
 
         return new SpaceTimeCoordinate(lat,lon,date)
     }
@@ -38,9 +56,10 @@ class LineParser(precision: Double = 0.1, timestep: Int = 3) extends java.io.Ser
 
     def parseDate(date: String, timestep: Int): Int = {
         // 2015-01-12 23:36:49
-        // 2015-02-01 00:13:14
         val formatter = DateTimeFormatter.ofPattern("uuuu-MM-DD HH:mm:ss")
-        return (DAYS.between(DATETIME_OFFSET, LocalDateTime.parse(date, formatter)) / timestep).toInt
+        //amount = start.until(end, MONTHS);
+        val tar = LocalDateTime.parse(date, formatter)
+        return (DATETIME_OFFSET.until(tar,DAYS) / timestep).toInt
     }
 
 }
